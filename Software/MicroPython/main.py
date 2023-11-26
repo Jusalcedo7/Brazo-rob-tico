f2f06ca44bf504559732121bfd116219d79f6cfc
Brayan Sebastian Vanegas Acero <57512279+MrBratian@users.noreply.github.com>
2023-11-25 20:21:52 -0500

    Update main.py
    added funtion .move to each servo in function exec

--- Software/MicroPython/main.py
+++ Software/MicroPython/main.py
@@ -2,39 +2,21 @@ import socket
 from servo import Servo
 from time import sleep
 
-'''
-motor = Servo(pin=2)
-while True:
-    motor.move(0)
-    sleep(1)
-    motor.move(90)
-    sleep(1)
-    motor.move(180)
-    sleep(1)
-    motor.move(90)
-    sleep(1)
-    motor.move(0)
-    sleep(1)
-    
 garra = Servo(pin=2)
-muñeca_pitch = Servo(pin=4)
-muñeca_yaw = Servo(pin=25)
-brazo = Servo(pin=26)
-antebrazo = Servo(pin=#)
-base = Servo(pin=#)
-'''
+muñeca_p = Servo(pin=4)
+muñeca_y = Servo(pin=5)
+brazo = Servo(pin=18)
+antebrazo = Servo(pin=19)
+base = Servo(pin=21)
 
 # Agregar nport de donde está el servidor TCP, en el ejemplo: 3000
 serverAddressPort = socket.getaddrinfo('0.0.0.0', 3000)[0][-1]
 # Cantidad de bytes a recibir
 bufferSize  = 128
-
 # Descomentar si el esp32 será una estación
 # from wifiSTA import connectSTA as connect
-
 # Descomentar si el esp32 estará en modo de acceso AP
 from wifiAP import apConfig as connect
-
 # poner acá el nombre de red ssid y password para conectarse
 connect("ARM_UNAL", "87654321")
 
@@ -50,34 +32,31 @@ def degrades_servo(data):
 
 def exec(data):
     print(data)
-    if identifier_servo(data) == "s7":
+    g = int(degrades_servo(data))
+    if identifier_servo(data) == "s1":
         print(degrades_servo(data))
-    elif identifier_servo(data) == "s6":
-        print(degrades_servo(data))
-    elif identifier_servo(data) == "s5":
-        print(degrades_servo(data))
-    elif identifier_servo(data) == "s4":
-        print(degrades_servo(data))
+        garra.move(g)
     elif identifier_servo(data) == "s2":
         print(degrades_servo(data))
-    elif identifier_servo(data) == "s1":
+        muñeca_p.move(g)
+    elif identifier_servo(data) == "s3":
         print(degrades_servo(data))
+        muñeca_y.move(g)
+    elif identifier_servo(data) == "s4":
+        print(degrades_servo(data))
+        brazo.move(g)
+    elif identifier_servo(data) == "s5":
+        print(degrades_servo(data))
+        antebrazo.move(g)
+    elif identifier_servo(data) == "s6":
+        print(degrades_servo(data))
+        base.move(g)
     else:
-        print("Otro")
+        print("Unknow data")
 
 sk = socket.socket()
 sk.bind(serverAddressPort)
-sk.listen(1)
-print("Listening on: ", serverAddressPort)
-
-while True:
-    conn, addr = sk.accept()
-    while True:
-        data = conn.recv(bufferSize)
-        # Si dato fue recibido, se decide que hacer con el.
-        if data:
-            exec(data)
-            # Con la siguiente instruccion se pueden enviar datos al
+	@@ -81,4 +73,4 @@ def exec(data):
             # dispositivo conectado
             conn.sendall("ok")
             # conn.send("ok")
