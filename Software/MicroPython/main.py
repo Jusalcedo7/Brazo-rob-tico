38fdf692bda4065c919e390b6a6e5f0609687cd3
Brayan Sebastian Vanegas Acero <57512279+MrBratian@users.noreply.github.com>
2023-11-27 16:56:21 -0500

    Update main.py

--- Software/MicroPython/main.py
+++ Software/MicroPython/main.py
@@ -1,6 +1,7 @@
 import socket
 from servo import Servo
 from time import sleep
+from wifiAP import apConfig as connect
 
 garra = Servo(pin=2)
 muñeca_p = Servo(pin=4)
@@ -9,18 +10,9 @@ brazo = Servo(pin=18)
 antebrazo = Servo(pin=19)
 base = Servo(pin=21)
              
-# Agregar nport de donde está el servidor TCP, en el ejemplo: 3000
 serverAddressPort = socket.getaddrinfo('0.0.0.0', 3000)[0][-1]
-# Cantidad de bytes a recibir
 bufferSize  = 128
 
-# Descomentar si el esp32 será una estación
-# from wifiSTA import connectSTA as connect
-
-# Descomentar si el esp32 estará en modo de acceso AP
-from wifiAP import apConfig as connect
-
-# poner acá el nombre de red ssid y password para conectarse
 connect("ARM_UNAL", "87654321")
 
 def identifier_servo(data):
@@ -66,11 +58,6 @@ while True:
     conn, addr = sk.accept()
     while True:
         data = conn.recv(bufferSize)
-        # Si dato fue recibido, se decide que hacer con el.
         if data:
             exec(data)
-            # Con la siguiente instruccion se pueden enviar datos al
-            # dispositivo conectado
-            conn.sendall("ok")
-            # conn.send("ok")
     conn.close()
